0c2a76ecf88e6b1525a4c54387ce6198650d939e
August-Rushme <864011713zqy@gmail.com>
2023-03-21 09:45:51 +0800

    init

---
+++ pages/template/template.vue
@@ -0,0 +1,30 @@
+<template>
+	<view>
+		<!-- #ifndef H5 -->
+		<z-nav-bar title="图片上传"></z-nav-bar>
+		<!-- #endif -->
+		<!-- 公共组件-每个页面必须引入 -->
+		<public-module></public-module>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			
+		};
+	},
+	//方法
+	methods: {
+		onPageJump(url) {
+			uni.navigateTo({
+				url: url
+			});
+		}
+	}
+};
+</script>
+<style lang="scss" scoped>
+@import '~@/style/mixin.scss';
+</style>
