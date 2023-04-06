220f46bb4140e53ad5ab96f8f64b004ed8001612
张莹 <2664745078@qq.com>
2023-04-07 00:28:39 +0800

    第一次提交健康管理模块代码-饮食记录

---
+++ pages/healthAssessment/beauty/beauty.vue
@@ -0,0 +1,33 @@
+<template>
+  <view>
+    美容
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    },
+    //方法
+    methods: {
+    
+      click(url, name) {
+        if (!url) {
+          this.$refs.uToast.default('开发中...')
+        }
+        uni.navigateTo({
+          url: url
+        });
+    
+      ;}
+    
+    },
+  }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ pages/healthAssessment/sleep/sleep.vue
@@ -0,0 +1,34 @@
+<template>
+  <view>
+    <z-nav-bar title="睡眠管理"></z-nav-bar>
+    <public-module></public-module>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    },
+    //方法
+    methods: {
+    
+      click(url, name) {
+        if (!url) {
+          this.$refs.uToast.default('开发中...')
+        }
+        uni.navigateTo({
+          url: url
+        });
+    
+      ;}
+    
+    },
+  }
+</script>
+
+<style lang="scss">
+
+</style>
