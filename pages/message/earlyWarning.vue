5ba675351298b9e70615959ba8da07448c441e2c
kurami <kurami.noya@gmail.com>
2023-04-25 18:45:14 +0800

    feat: 完成消息模块

--- pages/message/earlyWarning.vue
+++ pages/message/earlyWarning.vue
@@ -5,11 +5,39 @@
 
             <u-icon @click="goBack" class="ml-2" name="arrow-left" slot="left" color="#000000" size="24"></u-icon>
 
-
-            <u-icon class="mr-2" @click="goAddFriend" name="plus" slot="right" color="#000000" size="24"></u-icon>
+            <view class="mr-2" style="font-size: 30rpx; color: #5becbc;"  slot="right" >预警规则</view>
 
         </z-nav-bar>
-         <custom-tab-bar direction="horizontal" :show-icon="false" :selected="selected" @onTabItemTap="onTabItemTap" />
+          <u-tabs
+              :list="tabList"
+              lineWidth="140"
+              lineColor="#5becbc"
+			  class="tab_top"
+              :activeStyle="{
+                  color: '#303133',
+                  fontWeight: 'bold',
+                  transform: 'scale(1.05)'
+              }"
+              :inactiveStyle="{
+                  color: '#606266',
+                  transform: 'scale(1)'
+              }"
+              itemStyle="padding-left: 15px; padding-right: 15px; height: 34px; width: 50%"
+          >
+          </u-tabs>
+		  <view class="d-flex a-center j-center buttonGrounp my-1">
+		      <u-button  :hairline="hairline"  color="#71d5a1" text="签约医生" class="leftButton" :plain="true"
+		                ></u-button>
+		      <u-button  :hairline="hairline"  color="#71d5a1" text="关注医生" class="rightButton" :plain="true"
+		                ></u-button>
+		  </view>
+		  <view class="notification d-flex flex-column shadow border rounded m-1 p-2 bg-white" v-for="index in 5">
+		      <view class="notification_title my-1">
+		          <h3>血压</h3>
+		      </view>
+		          <view class="notification_text-content">低压 89 高压 112 心率 83</view>
+		          <view class="notification_text-content">2022-03-07 18:00:26</view>
+		  </view>
     </view>
 </template>
 <script setup>
@@ -29,3 +57,41 @@ export default {
     }
 }
 </script>
+<style lang="scss" scoped>
+.list-cell {
+  display: flex;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 10px 24rpx;
+  overflow: hidden;
+  color: #323233;
+  font-size: 14px;
+  line-height: 24px;
+  background-color: #fff;
+}
+
+.searchBar {
+
+}
+
+.buttonGrounp {
+
+  .leftButton {
+    border: 0;
+    width: 40%;
+    border-bottom-left-radius: 10px;
+    border-top-left-radius: 10px;
+  }
+
+  .rightButton {
+    border: 0;
+    width: 40%;
+    border-bottom-right-radius: 10px;
+    border-top-right-radius: 10px;
+  }
+}
+	
+.tab_top{
+	width: 100%;
+}
+</style>
