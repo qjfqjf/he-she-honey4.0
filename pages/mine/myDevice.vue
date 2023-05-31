b45192ade8beceff92a184b1e760a09d21df445f
张莹 <2664745078@qq.com>
2023-05-31 21:37:12 +0800

    设备

--- pages/mine/myDevice.vue
+++ pages/mine/myDevice.vue
@@ -35,12 +35,13 @@
 					img: require('@/static/icon/device/BTBP.png'),
 					title: '血压计',
 					detail: '(倍泰静态/动态血压计)',
+          url: '/pages/healthMonitor/bloodPressure/device/bloodPressureDevice1',
 				},
 				{
 					img: require('@/static/icon/device/JKBP.png'),
 					title: '血压计',
 					detail: '(佳康静态血压计)',
-					url: '/pages/template/deviceConnection',
+					url: '/pages/healthMonitor/bloodPressure/device/bloodPressureDevice2',
 					deviceInfo: {
 						deviceInfo: uni.getStorageSync('jkDeviceId'),
 						serviceId: '6E400001-B5A3-F393-E0A9-E50E24DCCA9E', //设备的服务值
