336ff7285606212e9ae6dc3043841af561abbf39
曲骏飞 <1781123785@qq.com>
2023-06-07 19:46:38 +0800

    feat:月报

--- pages/healthMonitor/oximeter/oximeterMonth.vue
+++ pages/healthMonitor/oximeter/oximeterMonth.vue
@@ -4,8 +4,8 @@
 		<z-nav-bar title="血氧月报">
 			<view slot="right" class="p-2" @click="handleDevelop">预警规则</view>
 		</z-nav-bar>
-		<public-module></public-module>
-		<TimeRage></TimeRage>
+		<public-module @getDate="getDate"></public-module>
+		<TimeRage @getDate="getDate"></TimeRage>
 		<view class="historyCard mb-3" v-for="(item,index) in dataList" :key="index">
 			<view class="top d-flex j-sb mb-2">
 				<view class="time">
@@ -26,51 +26,107 @@
 			</view>
 		</view>
 
+		<view>
+			<u-button @click="test">aaa</u-button>
+		</view>
+
 
 	</view>
 </template>
 
 <script>
 	import TimeRage from '../components/timeRage/TimeRage.vue'
+	import UButton from "../../../uni_modules/uview-ui/components/u-button/u-button.vue";
+	import dayjs from "../utils/dayjs"
+	import isBetween from 'dayjs/plugin/isBetween'
 	export default {
 		components: {
+			UButton,
 			TimeRage
 		},
 		data() {
 			return {
-
+				date:{
+					startTime:this.getFirstDayOfMonth().format('yyyy-MM-dd'),
+					endTime:this.getLastDayOfMonth().format('yyyy-MM-dd'),
+				},
+				allDataList:[],
 				dataList: [
 					{
-						time: "2023-3-29 15:30",
-						bloodOxygen: 168,
-						PI: 98,
-						rate: 81
+						test_time: "2023-3-20 15:30",
+						blood_oxygen: 168,
+						pi: 98,
+						pulse_rate: 81
 					},
 					{
-						time: "2023-3-29 15:30",
-						bloodOxygen: 168,
-						PI: 98,
-						rate: 81
+						test_time: "2023-4-29 15:30",
+						blood_oxygen: 168,
+						pi: 98,
+						pulse_rate: 81
 					},
 					{
-						time: "2023-3-29 15:30",
-						bloodOxygen: 168,
-						PI: 98,
-						rate: 81
+						test_time: "2021-3-29 15:30",
+						blood_oxygen: 168,
+						pi: 98,
+						pulse_rate: 81
 					},
 					{
-						time: "2023-3-29 15:30",
-						bloodOxygen: 168,
-						PI: 98,
-						rate: 81
+						test_time: "2023-3-27 15:30",
+						blood_oxygen: 168,
+						pi: 98,
+						pulse_rate: 81
 					}
 				]
 			};
 		},
+		created() {
+			dayjs.extend(isBetween)
+		},
 		onLoad() {
-			this.getHistoryList();
+			dayjs.extend(isBetween)
+			//this.getHistoryList();
+			//测试
+			// console.log(dayjs())
+			// console.log(dayjs('2016-10-30').isBetween('2016-01-01', '2016-10-30', 'day', '[]'))
+			// console.log(new Date(this.dataList[0].test_time).format('yyyy-MM-dd'))
+			// console.log(dayjs(new Date(this.dataList[0].test_time).format('yyyy-MM-dd')).isBetween(this.date.startTime,this.date.endTime,'day','[]'))
 		},
 		methods: {
+			//测试子组件传来的值是否是正确的
+			test(){
+				console.log("start"+this.date.startTime)
+				console.log("end"+this.date.endTime)
+			},
+			// 获取当前月的最后一天
+			getLastDayOfMonth() {
+				const now = new Date();
+				const year = now.getFullYear();
+				const month = now.getMonth() + 1; // 月份从0开始，需要加1
+				const lastDay = new Date(year, month, 0).getDate(); // 0表示上一个月的最后一天，即当前月的最后一天
+				const lastDayOfMonth = new Date(year, month - 1, lastDay);
+				return lastDayOfMonth;
+			},
+			// 获取当前月的第一天
+			getFirstDayOfMonth() {
+				const today = new Date();
+				const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+				return firstDayOfMonth
+			},
+
+			//从子组件获取开始和结束时间，并且更新数据
+			getDate(date){
+				this.date = date;
+				//清空数组内数据
+				this.allDataList = [];
+				//筛选出符合条件的数据
+				for(var i in this.allDataList){
+					//判断数据是否在所选日期范围内
+					if(dayjs(new Date(this.allDataList[i].test_time).format('yyyy-MM-dd')).isBetween(this.date.startTime,this.date.endTime, 'day', '[]')){
+						this.dataList.push(this.allDataList[i])
+					}
+				}
+			},
+
 			handleDevelop() {
 				this.$refs.uToast.show({
 					message: '开发中...'
@@ -91,9 +147,9 @@
 						"test_time"
 					]
 				}).then(res => {
-					this.dataList = res.result.records
+					this.allDataList = res.result.records
 				})
-			}
+			},
 
 		}
 	}
