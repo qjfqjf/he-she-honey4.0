9c862f1e6177b29bdaaff7e4ca368049d6712fd6
Thexingmeng <1920238209@qq.com>
2023-06-17 23:14:29 +0800

    服务0-1

---
+++ pages/educate/zhucan.vue
@@ -0,0 +1,43 @@
+<template>
+    <view class="content">
+      <z-nav-bar title="助残服务"></z-nav-bar>
+      <public-module></public-module>
+  
+    
+  
+    </view>
+  </template>
+  
+  <script>
+    export default {
+      components: {
+        
+      },
+      data() {
+        return {
+          baseList: []
+        };
+      },
+      //方法
+      methods: {
+        addSportsRecord() {
+          uni.navigateTo({
+            url: '/pages/healthManagement/sports/addSportsRecord'
+          })
+        },
+      },
+    }
+  </script>
+  
+  <style lang="scss">
+  .content{
+    background-color: #FFFFFF;
+    height: 100%;
+    .nothing{
+      text-align: center;
+      padding-top: 300rpx;
+      width: 400rpx;
+      margin: 0 auto;
+    }
+  }
+  </style>
