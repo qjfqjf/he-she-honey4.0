e35484a2a93182ad27bdbefbead03832db5b2984
tzyl <3316746427@qq.com>
2023-08-08 19:34:00 +0800

    fix

--- pages/homePage/homePage.vue
+++ pages/homePage/homePage.vue
@@ -2,72 +2,49 @@
 	<view class="container h-100 w-100 flex-column d-flex">
 		<public-module></public-module>
 		<z-nav-bar home title="数字健康管理" class="HomeNavBar" bg-color="#bef1d0" fontColor="black">
-			<img slot="left" :src="homePageIcons.Scanning.icon" class="small-icon p-2" alt="" @click="handleScan"/>
-			<img slot="right" :src="homePageIcons.Location.icon" class="small-icon p-2" alt=""/>
+			<img slot="left" :src="homePageIcons.Scanning.icon" class="small-icon p-2" alt="" @click="handleScan" />
+			<img slot="right" :src="homePageIcons.Location.icon" class="small-icon p-2" alt="" />
 		</z-nav-bar>
-		<view class="status-bar d-flex m-3 bg-white rounded-20">
+		<view class="status-bar d-flex m-3 bg-white rounded-20" v-if="currentItem !== null">
 			<view class="f-grow-1 flex-column h-50 px-2 py-1">
 				<view class="d-flex j-sb a-center">
 					<div class="d-flex j-center a-center ">
 						<img :src="homePageIcons.Bp.icon" class="medium-icon" alt="">
 						<span class="mx-1">
-							<h4 style="font-size: 30upx">{{this.savedData.name}}</h4>
+							<h4 style="font-size: 30upx">{{currentItem.name}}</h4>
 						</span>
 					</div>
 					<img :src="homePageIcons.ArrowUp.icon" style="width: 20upx" height="10upx" alt="">
 				</view>
-				<!-- <view class="d-flex j-sb a-center">
-					<view class="d-flex j-center a-center">
-						<h3 style="color: red; ">123</h3>
-					</view>
-					<h4 class="unit" style="color: red">mmHg</h4>
-				</view> -->
 			</view>
-
 			<view class="f-grow-1 flex-column h-50 px-2 py-1">
 				<view class="d-flex j-sb a-center">
 					<div class="d-flex j-center a-center">
 						<img :src="homePageIcons.Glu.icon" class="medium-icon" alt="">
 						<span class="mx-1">
-							<h6 style="color: green">{{this.savedData.value}}</h6>
-							<!-- <h4 style="font-size: 30upx">收缩压/舒张压</h4> -->
+							<h6 style="color: green">{{currentItem.value}}</h6>
+
 						</span>
 					</div>
 					<img :src="homePageIcons.ArrowDown.icon" style="width: 20upx" height="10upx" alt="">
 				</view>
-				<!-- <view class="d-flex j-sb a-center">
-					<view class="d-flex j-center a-center">
-						<h6 style="color: green">{{this.savedData.value}}</h6>
-					</view>
-					<h4 class="unit">mmHg</h4>
-				</view> -->
 			</view>
-
 			<view class="f-grow-1 flex-column h-50 px-2 py-1">
 				<view class="d-flex j-sb a-center">
 					<div class="d-flex j-center a-center">
 						<img :src="homePageIcons.UricAcid.icon" class="medium-icon" alt="">
 						<span class="mx-1">
-							<h4 style="font-size: 30upx">{{this.savedData.alert}}</h4>
+							<h4 style="font-size: 30upx">{{currentItem.eval}}</h4>
 						</span>
 					</div>
 				</view>
-				<!-- <view class="d-flex j-sb a-center">
-					<view class="d-flex j-center a-center">
-						<h3>279</h3>
-					</view>
-					<h4 class="unit">μmol/L</h4>
-				</view> -->
 			</view>
 		</view>
-
-
-
 		<view class="top-bar d-flex j-sb w-100 a-center mb-2 h-100">
-			<u-button class="leftRoundButton shadow h-100 shadow-lg border" @click="onPageJump('/pages/homePage/myUsers')"
-								style="z-index: 1">
+			<u-button class="leftRoundButton shadow h-100 shadow-lg border"
+				@click="onPageJump('/pages/homePage/myUsers')" style="z-index: 1">
 				<view class="rounded-circle bg-primary-dark m-1 w-50 h-50 roundButton d-flex a-center j-center"
-							style="background-color: rgb(6,158,193); color: aliceblue;"><span>用户</span></view>
+					style="background-color: rgb(6,158,193); color: aliceblue;"><span>用户</span></view>
 			</u-button>
 
 			<view class="scroll-container">
@@ -76,12 +53,9 @@
 
 			<u-button class="rightRoundButton shadow-lg border" @click="toCalendar" style="z-index: 1">
 				<view class="rounded-circle bg-success-dark m-1 w-50 h-50 roundButton d-flex a-center j-center"
-							style="background-color: #18b566; color: aliceblue;">提醒</view>
+					style="background-color: #18b566; color: aliceblue;">提醒</view>
 			</u-button>
 		</view>
-
-
-
 
 		<view class=" p-1">
 			<view class="w-100 rounded-20 " style="background-color: white">
@@ -90,8 +64,9 @@
 						<view class="d-flex">
 							<view class="d-flex f-grow-1 flex-column a-center j-center p-1" v-for="(item,index) in page"
 								:key="index">
-								<view @click="selectImg(page[index])"><img :src="item.icon" class="big-icon" alt=""/></view>
-								<view >
+								<view @click="selectImg(page[index])"><img :src="item.icon" class="big-icon" alt="" />
+								</view>
+								<view>
 									{{ item.name }}
 								</view>
 							</view>
@@ -100,8 +75,7 @@
 				</swiper>
 			</view>
 		</view>
-		<!-- <u-swiper class="swiper mx-1" :list="wisperImage" previousMargin="30" nextMargin="30" circular :autoplay="false"
-			radius="5" bgColor="#ffffff"></u-swiper> -->
+
 		<u-swiper class=" m-1" :list="wisperImage" indicator indicatorMode="line" circular :autoplay="false" radius="5"
 			bgColor="#ffffff">
 		</u-swiper>
@@ -113,72 +87,35 @@
 					<u-grid-item v-for="(listItem,listIndex) in appManage" :key="listIndex">
 						<navigator :url="listItem.path">
 							<u--image class="appManeger_block_icon" :src="listItem.icon"
-										:customStyle="{paddingLeft:15+'rpx'}" height="100upx" width="100upx">
-									</u--image>
+								:customStyle="{paddingLeft:15+'rpx'}" height="100upx" width="100upx">
+							</u--image>
 							<u--text :text="listItem.name" align="center"></u--text>
 						</navigator>
 					</u-grid-item>
 				</u-grid>
 
 				<u-gap height="20"></u-gap>
-
-<!--				<u-grid :border="false" col="2">-->
-<!--					<u-grid-item v-for="(listItem,listIndex) in otherFunction" :key="listIndex" >-->
-<!--						<navigator :url="listItem.path">-->
-<!--							<u&#45;&#45;image class="appManeger_block_icon" :src="listItem.icon"-->
-<!--												 height="130upx" width="300upx">-->
-<!--							</u&#45;&#45;image>-->
-<!--							<u&#45;&#45;text :text="listItem.name" align="center"></u&#45;&#45;text>-->
-<!--						</navigator>-->
-<!--					</u-grid-item>-->
-<!--				</u-grid>-->
-
-
-<!--				<u-grid :border="false" col="2">-->
-<!--					<u-grid-item v-for="(listItem, listIndex) in otherFunction" :key="listIndex">-->
-<!--						<navigator :url="listItem.path">-->
-<!--							<div style="position: relative;">-->
-<!--								<u-image class="appManeger_block_icon" :src="listItem.icon" height="130upx" width="300upx" style="border-radius: 20px;"></u-image>-->
-<!--								<u-text :text="listItem.name" align="center" style="position: absolute; bottom: 0; left: 0; right: 0; padding: 8px; border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; color: #fff;"></u-text>-->
-<!--							</div>-->
-<!--						</navigator>-->
-<!--					</u-grid-item>-->
-<!--				</u-grid>-->
-
-
-<!--				<u-grid :border="false" col="2">-->
-<!--					<u-grid-item v-for="(listItem, listIndex) in otherFunction" :key="listIndex">-->
-<!--						<div style="position: relative; display: inline-block;" @click="dev">-->
-<!--							<u-image class="appManeger_block_icon" :src="listItem.icon" height="130upx" width="300upx" style="border-radius: 10px;"></u-image>-->
-<!--							<text align="left" style="position: absolute; top: 44rpx; left: 20rpx; font-weight: bold; font-size: 16px; color: black;">{{listItem.name}}</text>-->
-<!--							<text align="left" style="position: absolute; top: 100rpx; left: 20rpx; font-weight: bold; font-size: 16px; color: black;">{{listItem.name}}</text>-->
-<!--						</div>-->
-<!--					</u-grid-item>-->
-<!--				</u-grid>-->
-
 				<u-grid :border="false" col="2">
 					<u-grid-item v-for="(listItem, listIndex) in otherFunction" :key="listIndex">
 						<div style="position: relative; display: inline-block;" @click="dev">
-							<u-image class="appManeger_block_icon" :src="listItem.icon" height="150upx"
-											 width="330upx" style="border-radius: 10px;">
-
+							<u-image class="appManeger_block_icon" :src="listItem.icon" height="150upx" width="330upx"
+								style="border-radius: 10px;">
 							</u-image>
 							<text align="left" style="position: absolute; top: 44rpx;
 							left: 20rpx; font-weight: bold; font-size: 30rpx; color: black;">{{ listItem.name }}
 							</text>
-							<button style="position: absolute; border-radius: 10px;
+							<button
+								style="position: absolute; border-radius: 10px;
 							height:40rpx ;width: 120rpx ;top: 110rpx; left: 20rpx;
 							font-size: 10rpx; color: rgb(0, 234, 141); background-color:rgb(189, 240, 207);  border: none;">{{ listItem.discription }}
 							</button>
 						</div>
 					</u-grid-item>
 				</u-grid>
-				<u-toast ref="uToast" />
+				<u-toast ref="uToast"/>
 			</view>
 		</view>
 		<z-navigation></z-navigation>
-
-
 	</view>
 </template>
 
@@ -212,40 +149,29 @@
 				homePageIcons,
 				otherFunction,
 				token: uni.getStorageSync('access-token'),
-				uid:'',
+				uid: '',
 				doctorId: 0,
 				userInfo: '',
 				defaultSelect: 0, //默认选中下标，从0开始
 				userList: [
-					// {
-					// 	images: '../../static/logo.png',
-					// 	name: '张淑芳'
-					// },
-					// {
-					// 	images: '../../static/logo.png',
-					// 	name: '王立群'
-					// },
+
 				],
 				// 当前用户
 				currentUser: {},
-
+				dataList: [], // Your list of data items
+				currentItemIndex: 0,
+				currentItem: null,
 			};
 		},
 		components: {
 			UButton,
 			UImage,
 			HeadImgList,
-
 		},
 		//第一次加载
 		onLoad(e) {
-
-			uni.$emit('callTargetMethod');
-			// 监听事件，在事件触发时接收数据参数并处理
-			uni.$on('callTargetMethod', () => {
-				this.savedData = uni.getStorageSync('firstData');
-				console.log(this.savedData);
-			});
+			this.userInfo = JSON.parse(uni.getStorageSync('userInfo'))
+			this.uid = this.userInfo.uid;
 			// 隐藏原生的tabbar
 			uni.hideTabBar();
 			//拿到用户列表
@@ -260,35 +186,50 @@
 		},
 		//页面显示
 		onShow() {
-			uni.$emit('callTargetMethod');
-			// 监听事件，在事件触发时接收数据参数并处理
-			uni.$on('callTargetMethod', () => {
-				this.savedData = uni.getStorageSync('firstData');
-				console.log(this.savedData);
-			});
-
 			// this.getUserList()
 			// 隐藏原生的tabbar
 			uni.hideTabBar();
 			// this.getUserList();
+			this.getAllHistoryList();
 
 			//判断蓝牙是否开启
 			this.openBlue();
 			this.userInfo = JSON.parse(uni.getStorageSync('userInfo'))
 			this.getRelationList()
-
 		},
+		created() {
+			// Start the timer to display data every 3 seconds
+			this.startDataDisplayTimer();
+		},
+
+
 		//方法
 		methods: {
-			selectImg(e){
-				this.appManage[4].icon="/"+e.icon
-				this.appManage[4].name=e.name;
-				this.appManage[4].path=e.path;
-				this.appManage[4].name = this.appManage[4].name+"服务"
+			startDataDisplayTimer() {
+				this.currentItem = this.dataList[this.currentItemIndex];
+				setInterval(this.changeDataItem, 5000);
 			},
-			toCalendar(){
+			changeDataItem() {
+				this.currentItemIndex = (this.currentItemIndex + 1) % this.dataList.length;
+				this.currentItem = this.dataList[this.currentItemIndex];
+			},
+			// 查询最新所有历史记录
+			getAllHistoryList() {
+				this.$http.post('/monitor/index', {
+					uid: 355,
+				}).then(res => {
+					this.dataList = res.data.data
+				})
+			},
+			selectImg(e) {
+				this.appManage[4].icon = "/" + e.icon
+				this.appManage[4].name = e.name;
+				this.appManage[4].path = e.path;
+				this.appManage[4].name = this.appManage[4].name + "服务"
+			},
+			toCalendar() {
 				uni.navigateTo({
-					url:"/pages/calendar/calendar",
+					url: "/pages/calendar/calendar",
 				})
 			},
 			openBlue() {
@@ -331,42 +272,7 @@
 						}
 					}
 				}
-				// uni.openBluetoothAdapter({
-				// 	success: (res) => { //已打开
-				// 		uni.getBluetoothAdapterState({ //蓝牙的匹配状态
-				// 			success: (row) => {
-				// 				console.log(row)
-				// 				// 开始搜索蓝牙设备
-				// 				// uni.startBluetoothDevicesDiscovery()
-				// 			},
-				// 			fail(error) {
-				// 				uni.showToast({
-				// 					icon: 'none',
-				// 					title: '查看手机蓝牙是否打开'
-				// 				});
-				// 			}
-				// 		});
-				//
-				// 	},
-				// 	fail: (err) => { //未打开
-				// 		uni.showModal({
-				// 			title: '提示',
-				// 			content: '请打开蓝牙以继续操作',
-				// 			success: function(res) {
-				// 				if (res.confirm) {
-				// 					uni.openSetting({
-				// 						success: function(res) {
-				// 							// 用户跳转到系统设置页面
-				// 						}
-				// 					})
-				// 				} else if (res.cancel) {
-				// 					console.log("用户点击取消！")
-				// 				}
-				// 			}
-				// 		});
-				//
-				// 	}
-				// })
+
 			},
 
 			// 获取亲属关系列表
@@ -377,7 +283,7 @@
 						uid: this.userInfo.uid,
 					})
 					.then((res) => {
-						console.log('res:',res)
+						console.log('res:', res)
 						this.userList = res.result.result.map((item) => {
 							return {
 								...item,
@@ -392,7 +298,7 @@
 				console.log()
 			},
 			//开发中。。。
-			dev(){
+			dev() {
 				uni.showToast({
 					title: "开发中...",
 					icon: "none"
@@ -437,22 +343,21 @@
 
 
 			changeHeadImg(index) {
-				// console.log(this.currentUser)
-				// console.log('当前选中' + index)
+
 				this.currentUser = this.userList[index]
 				//登录一下获取一下token
 				uni.request({
-							url: 'http://106.14.140.92:8881/platform/login',
-							method: 'post',
-							data: {
-								params: {
-									login:this.currentUser.login,
-									password:'123456'
-								},
-							},
-							success: (res) => {
-								uni.setStorageSync('access-token', res.data.result.data.token)
-							}
+					url: 'http://106.14.140.92:8881/platform/login',
+					method: 'post',
+					data: {
+						params: {
+							login: this.currentUser.login,
+							password: '123456'
+						},
+					},
+					success: (res) => {
+						uni.setStorageSync('access-token', res.data.result.data.token)
+					}
 				})
 
 				uni.setStorageSync('userInfo', JSON.stringify(this.currentUser))
@@ -504,6 +409,7 @@
 				// #endif
 			},
 		},
+
 		//页面隐藏
 		onHide() {},
 		//页面卸载
@@ -515,23 +421,24 @@
 		//用户点击分享
 		onShareAppMessage(e) {
 			return this.wxShare();
-		}
+		},
+
 	};
 </script>
 <style lang="scss" scoped>
-.top-bar {
-	left: 0;
-	right: 0;
-	top: 0;
-	z-index: 999;
-}
+	.top-bar {
+		left: 0;
+		right: 0;
+		top: 0;
+		z-index: 999;
+	}
 
-.scroll-container {
-	display: flex;
-	flex: 1;
-	justify-content: center;
-	overflow: hidden;
-}
+	.scroll-container {
+		display: flex;
+		flex: 1;
+		justify-content: center;
+		overflow: hidden;
+	}
 
 
 
